88999257747c56cb17890c656dc1b59fe1099825
Keita Yamamoto <kurometiko@gmail.com>
2015-02-11 22:27:35 +0900

    パーサー追加・作成中

---
+++ etc/evolution_purser.h
@@ -0,0 +1,34 @@
+﻿#ifndef __EVOLUTION_PURSER_H__
+#define __EVOLUTION_PURSER_H__
+
+#include "../evolution_utility.h"
+
+namespace EVOLUTION{
+    namespace UTILITY{
+
+        class Purser{
+        private:
+            const c8* cp_text;
+            u32 m_index;
+            c8* mp_buffer;
+            u32 m_buffer_count;
+        public:
+            Purser(u32 buffer_count);
+            ~Purser();
+
+
+            void SetText(c8* text);
+           
+            const c8* Search(const c8* key);
+
+            bool IsEnd();
+
+            void SetIndex(u32 index);
+
+            u32 GetIndex();
+        };
+        
+    }
+}
+
+#endif // !__EVOLUTION_PURSER_H__
